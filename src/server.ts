0a198e3884b98cd9ed1e10f9fa2ed5e04f1fe800
Léo Geison <leogeisom@gmail.com>
2026-04-28 22:06:47 -0300

    feat: implement env validation using zod

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app.js";
+import { app } from './app.js';
+import { env } from './env/index.js';
 
-app.listen( {
-  host: "0.0.0.0",
-  port: 3333,
-}).then(() => {
-  console.log("HTTP server running!");
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT
+  })
+  .then(() => {
+    console.log('HTTP server running!');
+  });
