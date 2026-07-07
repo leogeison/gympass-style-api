af20a8ceb630ca1ef0b98e744758430661b9b5a0
Léo Geison <leogeisom@gmail.com>
2026-07-07 18:22:22 -0300

    chore: configure Prisma 7

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import fastify from 'fastify';
 
 export const app = fastify();
+import { prisma } from '@/lib/prisma.js';
+
+prisma.user.create({
+  data: {
+    name: 'Leo',
+    email: 'leogeisom@gmail.com'
+  }
+});
---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+// src/lib/prisma.ts
+import { PrismaClient } from '@/generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { env } from '@/env/index.js';
+
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL
+});
+
+export const prisma = new PrismaClient({
+  adapter
+});
